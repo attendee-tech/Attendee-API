a1acb1d56ca74cdb4c4fa86cc9c377fe9853171a
attendee <attendee42@gmial.com>
2025-04-15 19:38:15 +0100

    second commit

--- Attendee/settings.py
+++ Attendee/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-31x@*$32+j+@rrapvhl72r5v4eqwy@ocg+luo(+kanyc%-6p20'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://attendee-api.onrender.com', 'http://127.0.0.1:8000']
 
 
 # Application definition
