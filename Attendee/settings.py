c06933c73b35ac5927588376f98c04d1e322e6fa
attendee-tech <attendee42@gmail.com>
2025-05-30 12:50:41 +0100

    db

--- Attendee/settings.py
+++ Attendee/settings.py
@@ -63,6 +63,7 @@ SIMPLE_JWT={
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -152,6 +153,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT='static'
 STATICFILES_DIRS=['static/assets',]
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
