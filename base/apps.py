31010db7cefcc8fe269f675a3b615544f0aa1b51
attendee-tech <attendee42@gmail.com>
2025-05-30 14:50:38 +0100

    apps.py

--- base/apps.py
+++ base/apps.py
@@ -1,23 +1,6 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
-import logging
-
-logger = logging.getLogger(__name__)
-
-def create_default_superuser():
-    User = get_user_model()
-    username = 'rahim'
-    email = 'admin@example.com'
-    password = 'admin.06'
-
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username, email, password)
-        logger.info(f"Superuser '{username}' created.")
-    else:
-        logger.info(f"Superuser '{username}' already exists.")
 
 class BaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'base'
-    def ready(self):
-        create_default_superuser()
+    
