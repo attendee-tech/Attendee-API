c307fb287409b1a50f1e92eaf4d53c68ba8dcf4f
attendee-tech <attendee42@gmail.com>
2025-05-30 13:04:25 +0100

    db

--- Attendee/settings.py
+++ Attendee/settings.py
@@ -104,13 +104,23 @@ WSGI_APPLICATION = 'Attendee.wsgi.application'
 
 
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
